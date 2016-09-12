d47c6cb41e6cafdfb44d166147fe16febe491832
Tom Bevan <tom.bevan@r3labs.io>
2016-09-12 15:27:51 +0100

    added formatting for Sprintf with aws region

--- mapper/instance.go
+++ mapper/instance.go
@@ -12,15 +12,15 @@ import (
 	"github.com/ernestio/aws-definition-mapper/output"
 )
 
-const SSMBootstrap = `
+var SSMBootstrap = `
 #!/bin/bash
 if which dpkg >/dev/null; then
 cd /tmp
-curl https://amazon-ssm-region.s3.amazonaws.com/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
+curl https://amazon-ssm-%s.s3.amazonaws.com/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
 dpkg -i amazon-ssm-agent.deb
 else
 cd /tmp
-curl https://amazon-ssm-region.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
+curl https://amazon-ssm-%s.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
 yum install -y amazon-ssm-agent.rpm
 fi`
 
