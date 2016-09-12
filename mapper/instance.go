7b825c195680ab13a909670de33ed964cc4df60b
Tom Bevan <tom.bevan@r3labs.io>
2016-09-12 15:09:16 +0100

    fixed formatting on ssm bootstrap script

--- mapper/instance.go
+++ mapper/instance.go
@@ -12,6 +12,18 @@ import (
 	"github.com/ernestio/aws-definition-mapper/output"
 )
 
+const SSMBootstrap = `
+#!/bin/bash
+if which dpkg >/dev/null; then
+cd /tmp
+curl https://amazon-ssm-region.s3.amazonaws.com/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
+dpkg -i amazon-ssm-agent.deb
+else
+cd /tmp
+curl https://amazon-ssm-region.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
+yum install -y amazon-ssm-agent.rpm
+fi`
+
 // MapInstances : Maps the instances for the input payload on a ernest internal format
 func MapInstances(d definition.Definition) []output.Instance {
 	var instances []output.Instance
@@ -38,18 +50,7 @@ func MapInstances(d definition.Definition) []output.Instance {
 			}
 
 			if d.Bootstrapping == "ssm" {
-				newInstance.UserData = `
-				#!/bin/bash
-				if which dpkg >/dev/null; then
-				    cd /tmp
-				    curl https://amazon-ssm-region.s3.amazonaws.com/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
-				    dpkg -i amazon-ssm-agent.deb
-				else
-				    cd /tmp
-				    curl https://amazon-ssm-region.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
-				    yum install -y amazon-ssm-agent.rpm
-				fi
-				`
+				newInstance.UserData = SSMBootstrap
 			}
 
 			instances = append(instances, newInstance)
